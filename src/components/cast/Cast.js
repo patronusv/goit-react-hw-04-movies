d36b5eeb96d0d658b3c02e7835345218b2d53000
Viktor Gorokhov <patronusv@gmail.com>
2021-01-24 17:48:54 +0200

    adds show all cast function

--- src/components/cast/Cast.js
+++ src/components/cast/Cast.js
@@ -8,13 +8,30 @@ const Cast = () => {
   const getMovieCast = async id => {
     const result = await api.fetchMovieCastById(id);
     console.log(result);
-    setState({ ...result });
+    setState({ ...result, castToShow: result.cast.slice(0, 10) });
   };
   useEffect(() => {
     getMovieCast(location.state.movieId);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <h2>{location.state.movieId}</h2>;
+  const showAllCast = () => {
+    console.log('before', state);
+
+    setState(prevState => ({ ...prevState, castToShow: [...prevState.cast] }));
+    console.log('after', state);
+  };
+  const { castToShow, cast } = state;
+  return (
+    <>
+      <h2>{location.state.movieId}</h2>
+      <ul>{castToShow && castToShow.map(item => <li key={item.cast_id}>{item.name}</li>)}</ul>
+      {cast.length !== castToShow.length && (
+        <button type="button" onClick={showAllCast}>
+          Show all
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Cast;
